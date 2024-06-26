4e9b8c3453da47b9c7ea96229730ea64e9fdad32
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-06-26 06:41:52 +0200

    logout on 401 error

--- src/core/data/fetcher.ts
+++ src/core/data/fetcher.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { LS, LSKeys } from '../local-store';
+import { signout } from '../login/signout';
 
 export const AX = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
@@ -11,15 +12,16 @@ if (token) {
   AX.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
-// AX.interceptors.response.use(
-//   response => response,
-//   (error: AxiosError) => {
-//     // whatever you want to do with the error
-//     const errors = (error.response?.data as ResponseError['data'])?.errors ?? {};
+AX.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    console.debug('errors', error.toJSON());
+    if (error.response?.status === 401) {
+      console.debug('AAAA');
+      signout();
+      window.location.replace('/login');
+    }
 
-//     isDev && console.debug('errors', error.toJSON());
-
-//     logoutOnError(errors);
-//     throw error;
-//   },
-// );
+    throw error;
+  },
+);
